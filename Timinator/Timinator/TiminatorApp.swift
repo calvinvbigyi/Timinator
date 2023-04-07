61d6bee95204d9cf869c4298ae7cad3eb077884d
Calvin Zhou <vbigyi@gmail.com>
2023-04-07 18:49:26 -0400

    Initial WIP for Timinator

---
+++ Timinator/Timinator/TiminatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TiminatorApp.swift
+//  Timinator
+//
+//  Created by Chengyi Zhou on 2023-04-05.
+//
+
+import SwiftUI
+
+@main
+struct TiminatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
